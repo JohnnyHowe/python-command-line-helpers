42473fc316291bb4acb1688b561ed240d888acb6
Johnny Howe <jonathon.o.howe@gmail.com>
2026-02-17 17:36:39 +1300

    Refactored input_cleaning out of namespace_formatter

--- input_cleaning.py
+++ input_cleaning.py
@@ -9,3 +9,9 @@ def replace_hypens_with_underscore(args: Namespace) -> None:
 	for name in attributess_with_hyphens:
 		new_name = name.replace('-', '_')
 		setattr(args, new_name, getattr(args, name))
+
+
+def unescape(value: str, literals_to_unescape=["\n"]) -> str:
+	for literal in literals_to_unescape:
+		value = value.replace("\\" + literal, literal)
+	return value
